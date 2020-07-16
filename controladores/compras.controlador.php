a311a0f93e8e6115fd0c64d0a3127bb5423e23e8
ches2409 <andresg2409@gmail.com>
2020-07-16 12:13:19 -0500

    actualizacion modulo compras - cantidades

--- controladores/compras.controlador.php
+++ controladores/compras.controlador.php
@@ -12,4 +12,63 @@ class ControladorCompras{
 
         return $respuesta;
     }
+
+    /*=====================================
+        CREAR COMPRA
+    =====================================*/ 
+
+    static public function ctrCrearCompra (){
+        
+        if(isset($_POST["nuevaCompra"])){
+
+            /*=====================================
+                ACTUALIZAR LAS VENTAS DEL PROVEEDOR, AUMENTAR EL STOCK Y AUMENTAR LAS COMPRAS DE LOS PRODUCTOS
+            =====================================*/ 
+
+            $listaProductosC = json_decode($_POST["listaProductosC"], true);
+            // var_dump($listaProductosC);
+
+            $totalProductosVendidos = array();
+
+            foreach($listaProductosC as $key => $value){
+
+                array_push($totalProductosVendidos, $value["cantidad"]);
+
+                $tablaProductos = "productos";
+
+                $item = "id";
+                $valor = $value["id"];
+                $orden = "id";
+
+                $traerProductoC = ModeloProductos::mdlMostrarProductos($tablaProductos, $item, $valor, $orden);
+
+                $item1a = "compras";
+                $valor1a = $value["cantidad"] + $traerProductoC["compras"];
+
+                $nuevasCompras= ModeloProductos::mdlActualizarProducto($tablaProductos, $item1a, $valor1a, $valor);
+
+                $item1b = "stock";
+                $valor1b = $value["stock"];
+
+                $nuevoStock = ModeloProductos::mdlActualizarProducto($tablaProductos, $item1b, $valor1b, $valor);
+
+
+
+            }
+
+            $tablaProveedores = "proveedores";
+
+            $item= "id";
+            $valor= $_POST["seleccionarProveedor"];
+
+            $traerProveedor = ModeloProveedores::mdlMostrarProveedores($tablaProveedores, $item, $valor);
+            // var_dump($traerProveedor["ventas"]);
+
+            $item1= "ventas";
+            $valor1 = array_sum($totalProductosVendidos) + $traerProveedor["ventas"];
+
+            $ventasProveedor = ModeloProveedores::mdlActualizarProveedor($tablaProveedores, $item1, $valor1, $valor);
+        }
+
+    }
 }
